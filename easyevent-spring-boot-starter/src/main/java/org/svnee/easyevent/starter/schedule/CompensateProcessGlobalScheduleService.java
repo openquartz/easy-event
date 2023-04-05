74b5310d5f5c83dfb416e073f40646a88a4c6a6e
svnlab <svnlab>
2023-04-05 20:53:05 +0800

    feat(Monitor支持): test

--- easyevent-spring-boot-starter/src/main/java/org/svnee/easyevent/starter/schedule/CompensateProcessGlobalScheduleService.java
+++ easyevent-spring-boot-starter/src/main/java/org/svnee/easyevent/starter/schedule/CompensateProcessGlobalScheduleService.java
@@ -26,6 +26,7 @@ public class CompensateProcessGlobalScheduleService implements InitializingBean 
 
     private ScheduledThreadPoolExecutor init(EventCompensateProcessProperty property,
         RejectedExecutionHandler rejectedExecutionHandler) {
+
         return new ScheduledThreadPoolExecutor(property.getThreadPoolCoreSize(),
             new ThreadFactoryBuilder()
                 .setNameFormat(property.getThreadPoolThreadPrefix() + "-global-thread-%d")
